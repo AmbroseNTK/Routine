7504a0b1ae59f70e2f6144e974f118f41e6cdaac
Nguyễn Tuấn Kiệt <nguyentuankiet23031998@gmail.com>
2020-09-02 21:20:02 +0700

    Change host and port

--- app.js
+++ app.js
@@ -1,31 +1,28 @@
-const express = require('express');
+const express = require("express");
 const app = express();
-const bodyParser = require('body-parser');
-const cors = require('cors');
+const bodyParser = require("body-parser");
+const cors = require("cors");
 
-const config = require('./config');
-const admin = require('firebase-admin');
+const config = require("./config");
+const admin = require("firebase-admin");
 const serviceAccount = require("./key/key.json");
 
-function init(){
-    app.use(bodyParser());
-    app.use(cors());
-    app.use(require('./middleware/log'));
-    admin.initializeApp({
-        credential: admin.credential.cert(serviceAccount),
-        databaseURL: "https://routino-app.firebaseio.com"
-    });
-    // Assign routers zone
-    app.use("/v1/users",require('./routers/v1/users'));
-    app.use("/v1/tasks",require('./routers/v1/task'));
-    // End of routers zone
+function init() {
+  app.use(bodyParser());
+  app.use(cors());
+  app.use(require("./middleware/log"));
+  admin.initializeApp({
+    credential: admin.credential.cert(serviceAccount),
+    databaseURL: "https://routino-app.firebaseio.com",
+  });
+  // Assign routers zone
+  app.use("/v1/users", require("./routers/v1/users"));
+  app.use("/v1/tasks", require("./routers/v1/task"));
+  // End of routers zone
 
-    app.listen(8080,"127.0.0.1",()=>{
-        console.log("Server is running");
-    });
+  app.listen(1080, "0.0.0.0", () => {
+    console.log("Server is running");
+  });
 }
 
 init();
-
-
-
